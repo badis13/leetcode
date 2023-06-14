c07de4c7f64563320b2d308847325e26a49bff4e
= <=>
2023-06-14 20:44:59 +0500

    задачка

---
+++ medium/reverse-integer/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	fmt.Println(reverse(123456))
+}
+
+const (
+	min = -1 << 31
+	max = 1<<31 - 1
+)
+
+func reverse(x int) int {
+	d := 0
+	isNegative := x < 0
+	if isNegative {
+		x = -x
+	}
+	for ; x > 0; x /= 10 {
+		b := x % 10
+		d = (d * 10) + b
+	}
+	if isNegative {
+		d = -d
+	}
+	if d < min || d > max {
+		return 0
+	}
+	return d
+}
