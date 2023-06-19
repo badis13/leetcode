6af808e4559e90eb23c164e838eaa25810e18169
= <=>
2023-06-20 00:13:09 +0500

    Задачки

---
+++ easy/3/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+// https://leetcode.com/problems/add-two-integers/
+func main() {
+
+	fmt.Println(sum(5, 5))
+}
+
+func sum(num1 int, num2 int) int {
+	return num1 + num2
+}
