3f5b2daec704efc4e99034bff875d81fcdb588f4
= <=>
2023-06-20 15:53:02 +0500

    задачка

---
+++ easy/10/main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+// https://leetcode.com/problems/two-sum/
+
+func main() {
+	a := []int{1, 4, 2, 65, 3}
+	b := 5
+	fmt.Println(twoSum(a, b))
+}
+
+func twoSum(nums []int, target int) []int {
+	indices := make(map[int][]int)
+	for i := 0; i < len(nums); i++ {
+		indices[nums[i]] = append(indices[nums[i]], i)
+	}
+
+	for num, numIndices := range indices {
+		pairNum := target - num
+		if num == pairNum {
+			if len(numIndices) > 1 {
+				return numIndices[:2]
+			}
+			continue
+
+		}
+
+		pairIndices := indices[pairNum]
+		if len(pairIndices) > 0 {
+			return []int{numIndices[0], pairIndices[0]}
+		}
+	}
+
+	return nil
+}
