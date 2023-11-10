3b327862b2d845a7c2dac29740e5f499b2690949
badis13 <badis13@mail.ru>
2023-11-10 06:32:21 -0500

    new solution

--- medium/2/3/main.go
+++ medium/2/3/main.go
@@ -1,72 +1,81 @@
 package main
 
 import (
+	"container/heap"
 	"fmt"
 )
 
 // heap solution https://leetcode.com/problems/top-k-frequent-words
 func main() {
-	words := []string{"plpaboutit", "jnoqzdute", "sfvkdqf", "mjc", "nkpllqzjzp", "foqqenbey", "ssnanizsav", "nkpllqzjzp", "sfvkdqf", "isnjmy", "pnqsz", "hhqpvvt", "fvvdtpnzx", "jkqonvenhx", "cyxwlef", "hhqpvvt", "fvvdtpnzx", "plpaboutit", "sfvkdqf", "mjc", "fvvdtpnzx", "bwumsj", "foqqenbey", "isnjmy", "nkpllqzjzp", "hhqpvvt", "foqqenbey", "fvvdtpnzx", "bwumsj", "hhqpvvt", "fvvdtpnzx", "jkqonvenhx", "jnoqzdute", "foqqenbey", "jnoqzdute", "foqqenbey", "hhqpvvt", "ssnanizsav", "mjc", "foqqenbey", "bwumsj", "ssnanizsav", "fvvdtpnzx", "nkpllqzjzp", "jkqonvenhx", "hhqpvvt", "mjc", "isnjmy", "bwumsj", "pnqsz", "hhqpvvt", "nkpllqzjzp", "jnoqzdute", "pnqsz", "nkpllqzjzp", "jnoqzdute", "foqqenbey", "nkpllqzjzp", "hhqpvvt", "fvvdtpnzx", "plpaboutit", "jnoqzdute", "sfvkdqf", "fvvdtpnzx", "jkqonvenhx", "jnoqzdute", "nkpllqzjzp", "jnoqzdute", "fvvdtpnzx", "jkqonvenhx", "hhqpvvt", "isnjmy", "jkqonvenhx", "ssnanizsav", "jnoqzdute", "jkqonvenhx", "fvvdtpnzx", "hhqpvvt", "bwumsj", "nkpllqzjzp", "bwumsj", "jkqonvenhx", "jnoqzdute", "pnqsz", "foqqenbey", "sfvkdqf", "sfvkdqf"}
-	k := 1
+	words := []string{"i", "love", "leetcode", "i", "love", "coding"}
+	k := 2
 	fmt.Println(topKFrequent(words, k))
 }
 
 type WordFrequent struct {
 	word     string
 	frequent int
+	index    int
 }
 
+type Heap []*WordFrequent
+
 func topKFrequent(words []string, k int) []string {
-	frequencies := make(map[string]int)
+	var result []string
+	frequencies := make(map[string]int, len(words))
 	for i := 0; i < len(words); i++ {
 		frequencies[words[i]]++
 	}
 
-	current := make([]WordFrequent, 0, len(frequencies))
-	for word, frequency := range frequencies {
-		current = append(current, WordFrequent{word: word, frequent: frequency})
+	wfi := make(Heap, 0, len(frequencies))
+
+	i := 0
+	for value, priority := range frequencies {
+		heap.Push(&wfi, WordFrequent{
+			word:     value,
+			frequent: priority,
+			index:    i,
+		})
+		i++
 	}
 
-	current = newHeap(current)
-
-	result := make([]string, k)
 	for i := 0; i < k; i++ {
-		result[i] = current[i].word
+		item := heap.Pop(&wfi).(*WordFrequent)
+		result = append(result, item.word)
 	}
 
 	return result
 }
 
-func newHeap(arr []WordFrequent) []WordFrequent {
-	var leftChild, rightChild, i, j int
-
-	for largestChild := 0; (i < len(arr)) && (j < len(arr)); {
-		leftChild = 2*i + 1
-		rightChild = 2*i + 2
-		largestChild = i
-
-		if (leftChild < len(arr)) && (arr[leftChild].frequent > arr[largestChild].frequent) {
-			temp := arr[largestChild]
-			arr[largestChild] = arr[leftChild]
-			arr[leftChild] = temp
-			continue
-		}
-
-		if (rightChild < len(arr)) && (arr[rightChild].frequent > arr[largestChild].frequent) {
-			temp := arr[largestChild]
-			arr[largestChild] = arr[rightChild]
-			arr[rightChild] = temp
-			continue
-
-		}
-
-		if largestChild == i {
-			i++
-			continue
-		}
-
+func (h Heap) Less(i, j int) bool {
+	if h[i].frequent == h[j].frequent {
+		return h[i].word < h[j].word
 	}
 
-	return arr
+	return h[i].frequent > h[j].frequent
+}
 
+func (h Heap) Len() int {
+	return len(h)
+}
+
+func (h Heap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+
+func (h *Heap) Push(x any) {
+	n := len(*h)
+	item := x.(WordFrequent)
+	item.index = n
+	*h = append(*h, &item)
+}
+
+func (h *Heap) Pop() any {
+	old := *h
+	n := len(old)
+	item := old[n-1]
+	old[n-1] = nil
+	item.index = -1
+	*h = old[0 : n-1]
+	return item
 }
