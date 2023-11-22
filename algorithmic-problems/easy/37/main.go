8949a8ad367787505f2797d909d1aecbf8d86e7d
badis13 <badis13@mail.ru>
2023-11-22 06:15:32 -0500

    source

--- algorithmic-problems/easy/37/main.go
+++ algorithmic-problems/easy/37/main.go
@@ -2,6 +2,8 @@ package main
 
 import "fmt"
 
+//https://leetcode.com/problems/concatenation-of-array/submissions/
+
 func main() {
 	nums := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 15}
 	fmt.Println(getConcatenation(nums))
