215bcdea973f4058b9423beee860bf5266cfcf6e
badis13 <badis13@mail.ru>
2023-11-28 06:39:10 -0500

    modif sol

--- algorithmic-problems/easy/24/main.go
+++ algorithmic-problems/easy/24/main.go
@@ -1,6 +1,13 @@
 package main
 
+import "fmt"
+
 // https://leetcode.com/problems/n-ary-tree-preorder-traversal/
+
+type Node struct {
+	Val      int
+	Children []*Node
+}
 
 func curPreorder(root *Node, res []int) []int {
 	res = append(res, root.Val)
@@ -19,5 +26,15 @@ func preorder(root *Node) []int {
 }
 
 func main() {
-
+	var head1 Node
+	var child1 Node
+	var child2 Node
+	var child3 Node
+	head1.Val = 5
+	child1.Val = 2
+	child2.Val = 8
+	child3.Val = 2
+	var head1Arr []*Node
+	head1.Children = head1Arr
+	fmt.Println(preorder(&head1))
 }
