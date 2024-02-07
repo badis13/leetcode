5e22ad1a92c2acefb02aca198493cd5585a1b08c
vboxuser <vboxuser@test2.myguest.virtualbox.org>
2024-02-07 13:28:20 +0500

    new sql sol

---
+++ postgreSQL/13/main.sql
@@ -0,0 +1,5 @@
+--https://leetcode.com/problems/employee-bonus/description/
+
+SELECT t1.name, t2.bonus
+FROM Employee AS t1 LEFT JOIN Bonus AS t2 ON t1.empId = t2.empId
+WHERE t2.bonus < 1000 OR t2.bonus IS NULL
