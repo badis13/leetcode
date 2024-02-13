6e4841cc112fffc1324aac57098440c3077c1652
badis13 <badis13@mail.ru>
2024-02-13 20:04:39 +0500

    add link

--- postgreSQL/16/main.sql
+++ postgreSQL/16/main.sql
@@ -1,3 +1,4 @@
+--https://leetcode.com/problems/combine-two-tables/submissions/1174240246/
 SELECT t1.firstName, t1.lastName, t2.city, t2.state
 FROM Person AS t1 LEFT JOIN Address AS t2
 ON t2.personId = t1.personId
